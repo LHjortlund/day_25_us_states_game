a141b41b6a72c334fa505acbc3b41ba7f9733800
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-16 09:51:43 +0200

    update

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/ 50 States correct ", prompt="What's another state name?").title()
+    print(answer_state)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?")
-print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(state_data.x.item(), state_data.y.item())
-    t.write(state_data.state.item())
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+#states to learn.csv
+
 
 #if answer_state is one of the states in all the states of the 50_states.csv
     #if they got it right:
